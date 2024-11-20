a0d6b83e10c2192629c5e4aea178eba86691fbd5
Muqadas Shehzadi <muqadasshahzedi@gmail.com>
2024-11-20 21:27:28 +0000

    JavaScript config

--- JavaScript/Test.js
+++ JavaScript/Test.js
@@ -1,1 +1,9 @@
-console.log("Muqadas Here")
+const accountId = 14453
+let account = "muqadas@google.com"
+var accountPassword = "12345"
+// account  = "jaipur"
+/* don't use var */
+
+console.log(account);
+console.table([accountId,accountPassword,account])
+alert
